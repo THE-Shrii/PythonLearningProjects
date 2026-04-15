268c8bdcca4d8ca23b233d86c2305143573d1f65
Shristi Kumari <shristikumari1910@gmail.com>
2026-04-16 00:20:51 +0530

    string reverse

---
+++ pro1.py
@@ -0,0 +1,101 @@
+# ✅ 1. Even-Odd Checker
+# 🎯 What it does:
+# User enters a number → program tells Even or Odd
+
+# num = int(input("Enter a number: "))
+#
+# if (num % 2 == 0):
+#     print("even")
+# else:
+#     print("odd")
+
+
+
+
+
+
+# ✅ 2. Simple Calculator
+# 🎯 What it does:
+# Performs +, -, *, /
+
+# num1 = int(input("Enter first one: "))
+# num2 = int(input("Enter second two: "))
+# operation = input("Enter operation(+, -, *, /): ")
+#
+# if operation == "+":
+#     print(num1+num2)
+# elif operation == "-":
+#     print(num1-num2)
+# elif operation == "*":
+#     print(num1*num2)
+# elif operation == "/":
+#     print(num1/num2)
+# else:
+#     print("Operation not supported")
+
+
+
+
+# ✅ 3. Largest of 3 Numbers
+# 🎯 What it does:
+# Find biggest number
+#
+#
+# input1 = int(input("Enter 1st no.:"))
+# input2 = int(input("Enter 2nd no.:"))
+# input3 = int(input("Enter 3rd no.:"))
+#
+# if input1 > input2 and input1 > input3:
+#     print("1st Number is biggest")
+# elif input2 > input1 and input2 > input3:
+#     print("2nd Number is biggest")
+# elif input3 > input1 and input3 > input2:
+#     print("3rd Number is biggest")
+# else:
+#     print("All numbers are equal or two numbers are same")
+#
+#  FOR EQUAL NUMBERS
+
+
+# input1 = int(input("Enter 1st no.:"))
+# input2 = int(input("Enter 2nd no.:"))
+# input3 = int(input("Enter 3rd no.:"))
+#
+# if input1 == input2 == input3:
+#     print("All numbers are equal")
+#
+# elif input1 == input2:
+#     print("1st and 2nd numbers are equal")
+#
+# elif input2 == input3:
+#     print("2nd and 3rd numbers are equal")
+#
+# elif input1 == input3:
+#     print("1st and 3rd numbers are equal")
+#
+# elif input1 > input2 and input1 > input3:
+#     print("1st Number is biggest")
+#
+# elif input2 > input1 and input2 > input3:
+#     print("2nd Number is biggest")
+#
+# else:
+#     print("3rd Number is biggest")
+
+
+
+
+
+# ✅ 4. String Reverser
+# 🎯 What it does:
+#
+# Reverse a word
+
+# word = input("Enter a word: ")
+# print(word[::-1])
+
+
+
+
+
+
