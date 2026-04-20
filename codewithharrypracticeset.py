6d65cda8f7eb806b706934d9755d58370950d8c0
Shristi Kumari <shristikumari1910@gmail.com>
2026-04-20 23:15:20 +0530

    cwh

---
+++ codewithharrypracticeset.py
@@ -0,0 +1,54 @@
+# PROGRAM TO ADD 2 NUMBERS
+# a = 10
+# b =10
+# print(a+b)
+# print(a-b)
+# print(a*b)
+
+
+# PROGRAM TO FIND REMINDER WHEN A NO IS DIVIDED BY Z
+# a = 10
+# b = 2
+# print("Remainder when a is divide by b:", a % b)
+
+
+# CHECK THE TYPE OF VARIABLES ASSIGNED USING INPUT FUNCTION
+
+# a = input("enter value of a: ")
+# print(type(a))
+
+
+# USE COMPARISON OPERATOR /
+# a = int(input("Enter the first number: "))
+# b = int(input("Enter the second number: "))
+# print("A is greater than B" , a > b)
+# print("A is less than B", a < b)
+
+
+# PROGRAM TO FIND AVERAGE OF TWO NUMBERS
+# a = int(input("Enter the first number: "))
+# b = int(input("Enter the second number: "))
+# print("AVERAGE:",(a + b )/2)
+
+
+# SQAURE OF NUMBER
+# a = int(input("Enter the  number: "))
+# print("Square of no. is:", a**2 , "& Cube of no. is:", a**3)
+
+
+
+# CH -3
+# DISPLAY USER ENTERED NAME FOLLOWED BY WELCOME MESSAGE
+# name = input("Enter your name: ")
+# print(f"Hello {name}, welcome to Python!")
+
+#fill in a letter template
+# letter = '''Dear <|Name|>,
+# You are selected on
+# <|Date|>'''
+# print(letter.replace('<|Name|>','Shristi').replace("<|Date|>","19 May"))
+
+# DETECT DOUBLE SPACE IN PARA
+# name = "classy chic  "
+# print(name.find("  "))
+
